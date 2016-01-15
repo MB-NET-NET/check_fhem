e824856a81c6b1682bac0737e32c5366be8c5a84
Michael Bussmann <bus@mb-net.net>
2016-01-15 09:14:52 +0100

    Minor logging via syslog

--- cul.sh
+++ cul.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 DEV=/dev/ttyACM0
+PORT=2323
 
 while (true)
 do
-	/usr/bin/socat -ly GOPEN:$DEV,raw,echo=0 TCP-LISTEN:2323
-	sleep 1
+    	if [ -e $DEV ];
+	then
+	    	logger "CUL relay started. $DEV via $PORT"
+		/usr/bin/socat -ly GOPEN:$DEV,raw,echo=0 TCP-LISTEN:$PORT
+		logger "socat exited.  Sleeping..."
+	else
+	    	logger "$DEV not found.  Waiting for device to appear..."
+	fi
+	sleep 10
 done
